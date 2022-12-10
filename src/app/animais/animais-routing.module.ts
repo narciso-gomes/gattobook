6fdda0787b949d1ad7a2a839a72ca518d7e8b306
Narciso Gomes <narcisogomes@MacBook-Air-de-Narciso.local>
2022-12-10 19:10:51 -0300

    ADD: list-animais component

--- src/app/animais/animais-routing.module.ts
+++ src/app/animais/animais-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListaAnimaisComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AnimaisRoutingModule { }
+export class AnimaisRoutingModule {}
--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
@@ -10,13 +12,16 @@ export class LoginComponent implements OnInit {
   usuario = '';
   senha = '';
 
-  constructor(private autenticacaoService: AutenticacaoService) {}
+  constructor(
+    private autenticacaoService: AutenticacaoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   login() {
     this.autenticacaoService.autenticar(this.usuario, this.senha).subscribe({
-      next: () => console.log('Autenticado com sucesso'),
+      next: () => this.router.navigate(['animais']),
       error: (e) => {
         alert('Usuário ou senha inválido');
         console.log('error');
