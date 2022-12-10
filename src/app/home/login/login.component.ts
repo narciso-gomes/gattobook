039a8a0406d1c233437ed083f7c96a8c51582656
Narciso Gomes <narcisogomes@MacBook-Air-de-Narciso.local>
2022-12-10 17:04:25 -0300

    ADD: request autenticacao

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
 
-  constructor() { }
+  constructor(private autenticacaoService: AutenticacaoService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.autenticacaoService.autenticar(this.usuario, this.senha).subscribe({
+      next: () => console.log('Autenticado com sucesso'),
+      error: (e) => {
+        alert('Usuário ou senha inválido');
+        console.log('error');
+      },
+    });
   }
-
 }
