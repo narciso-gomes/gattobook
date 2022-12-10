ef8d09b36278c25f58651c9f7d232ee88d1f8ad3
Narciso Gomes <narcisogomes@MacBook-Air-de-Narciso.local>
2022-12-10 19:56:10 -0300

    ADD: form template driven validations

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
-import { LoginComponent } from './login/login.component';
+import { HomeRoutingModule } from 'src/app/home/home-routing.module';
+import { HomeComponent } from 'src/app/home/home.component';
+import { LoginComponent } from 'src/app/home/login/login.component';
+import { MensagemModule } from 'src/app/componentes/mensagem/mensagem.module';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NovoUsuarioComponent
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    FormsModule
+    FormsModule,
+    MensagemModule
   ]
 })
 export class HomeModule { }
