b9fdf74de2c9ba98d6fb9b6eb743430e3450da2e
Narciso Gomes <narcisogomes@MacBook-Air-de-Narciso.local>
2022-12-10 17:10:23 -0300

    ADD: lista animais component

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AnimaisRoutingModule } from './animais-routing.module';
+import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListaAnimaisComponent
+  ],
   imports: [
     CommonModule,
     AnimaisRoutingModule
